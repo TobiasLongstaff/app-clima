167d0da6ca6452f22b810912021d3e0738e6801e
Tobias Longstaff <tobilongstaff@outlook.com>
2022-08-11 00:52:00 -0300

    v3.0.0

--- src/scripts.js
+++ src/scripts.js
@@ -1,45 +1,84 @@
-const APPKEY = 'b3793fa27f3523785e39456380d43fbf'
-const url = 'https://api.openweathermap.org'
+const APPKEY_openweather = 'b3793fa27f3523785e39456380d43fbf'
+const APPKEY_positionstrack = '1ca1073357cef86919c12fba6bb02333'
+const url_openweather = 'https://api.openweathermap.org/data/2.5/'
+const url_positionstack = 'http://api.positionstack.com/v1/'
 
 const inputCountries = document.querySelector('#search')
 const units = document.querySelector('#units')
 
 inputCountries.addEventListener('change', () => {
-    if(inputCountries.value) getWeather(inputCountries.value, localStorage.getItem('units'))
+    if(inputCountries.value) {
+        document.querySelector('#loader').style.display = 'flex'
+        document.querySelector('#app').style.opacity = 0
+        getWeather(inputCountries.value, localStorage.getItem('units'))
+    }
 })
 
 units.addEventListener('change', () => {
+    document.querySelector('#loader').style.display = 'flex'
+    document.querySelector('#app').style.opacity = 0
     getWeather(localStorage.getItem('country'), units.value)
 })
 
-const getWeather = async (country = 'Buenos%20Aires,ar', units = 'metric') => {
+const getWeather = async (country = 'Buenos Aires', units = 'metric') => {
     localStorage.setItem('country', country)
     localStorage.setItem('units', units)
     try {
-        const response = await fetch(`${url}/data/2.5/weather?q=${country}&units=${units}&APPID=${APPKEY}`)
+        const responsePosition = await fetch(`${url_positionstack}forward?query=${country}&limit=1&access_key=${APPKEY_positionstrack}`)
+        const ubication = await responsePosition.json()
+        console.log(ubication)
+        const response = await fetch(
+            `${url_openweather}onecall?lat=${ubication.data[0].latitude}&lon=${ubication.data[0].longitude}&units=${units}&exclude=minutely&APPID=${APPKEY_openweather}`
+        )
         const weather = await response.json()
-        document.querySelector('#city').innerHTML = `${weather.name} ${weather.sys.country}` 
-        document.querySelector('#temp').innerHTML = `${weather.main.temp = Math.round(weather.main.temp)}°`
-        document.querySelector('#feels-like').innerHTML = `${weather.main.feels_like = Math.round(weather.main.feels_like)}°`
-        document.querySelector('#humidity').innerHTML = `${weather.main.humidity}%`
-        document.querySelector('#wind-speed').innerHTML = `${weather.wind.speed}km/h`
-        document.querySelector('#img-weather').src = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
-        document.querySelector('#img-weather').alt = `${weather.weather[0].description}`
-        document.querySelector('#desciption-weather').innerHTML = `${weather.weather[0].main}` 
-        
-        const rootElem=document.querySelector(':root')
+        document.querySelector('#city').innerHTML = country 
+        document.querySelector('#temp').innerHTML = `${Math.round(weather.current.temp)}°`
+        document.querySelector('#feels-like').innerHTML = `${Math.round(weather.current.feels_like)}°`
+        document.querySelector('#humidity').innerHTML = `${weather.current.humidity}%`
+        document.querySelector('#wind-speed').innerHTML = `${weather.current.wind_speed}km/h`
+        document.querySelector('#img-weather').src = `https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`
+        document.querySelector('#img-weather').alt = `${weather.current.weather[0].description}`
+        document.querySelector('#desciption-weather').innerHTML = `${weather.current.weather[0].main}`
 
-        if(weather.weather[0].icon.includes('d')) {
+        document.querySelector('#loader').style.display = 'none'
+        document.querySelector('#app').style.opacity = 1
+
+        const rootElem = document.querySelector(':root')
+        if(weather.current.weather[0].icon.includes('d')) {
             rootElem.style.setProperty('--background', '#fff')
             rootElem.style.setProperty('--primary', '#d4dbfb')
             rootElem.style.setProperty('--font', '#131121')
-
         }
         else {
             rootElem.style.setProperty('--background', '#131121')
             rootElem.style.setProperty('--primary', '#1C1A2E')
             rootElem.style.setProperty('--font', '#FFFFFF')
         }
+
+        let template = ''
+
+        // <label>${new Date(temp.dt*1000)}</label>
+
+        weather.hourly.map(temp => {
+            template += 
+            `
+                <div class="card-weather">
+                    <div class="container-img-card">
+                        <img class="img-day-weather" src="https://openweathermap.org/img/wn/${temp.weather[0].icon}@2x.png" alt="${temp.weather[0].description}"/>                    
+                    </div>
+                    <h3>${Math.round(temp.temp)}°</h3>
+                    <labe class="text-datails-temp">${new Date(temp.dt*1000).getHours()}:00</labe>
+                </div>
+            `
+
+            // console.log([
+            //     Math.round(temp.temp),
+            //     temp.humidity,
+            //     temp.wind_speed,
+            //     temp.weather[0].icon
+            // ])
+        })
+        document.querySelector('#weather-forecast').innerHTML = template
     }
     catch(error) {
         console.log(error)
